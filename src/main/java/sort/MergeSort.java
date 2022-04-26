c680bc82eb3ddd2370e5f21307916b07805d5205
rMieep <Alexander-Schnackenberg@web.de>
2022-04-26 21:46:49 +0200

    implement merge sort

---
+++ src/main/java/sort/MergeSort.java
@@ -0,0 +1,51 @@
+package sort;
+
+public class MergeSort implements SortAlgorithm {
+
+    @Override
+    public void sort(int[] array) {
+        sort(array, 0, array.length - 1);
+    }
+
+    private void sort(int[] array, int left, int right) {
+        if(left < right) {
+            int middle = (left + right) / 2;
+
+            sort(array, left, middle);
+            sort(array, middle + 1, right);
+            merge(array, left, middle, right);
+        }
+    }
+
+    private void merge(int[] array, int left, int middle, int right) {
+        int mergedIndex = left;
+        int leftPartitionIndex = 0;
+        int rightPartitionIndex = middle + 1;
+        int[] leftPartition = new int[middle - left + 1];
+        System.arraycopy(array, left, leftPartition, 0, middle - left + 1);
+
+        while(leftPartitionIndex < leftPartition.length && rightPartitionIndex <= right) {
+            if(leftPartition[leftPartitionIndex] < array[rightPartitionIndex]) {
+                array[mergedIndex] = leftPartition[leftPartitionIndex];
+                mergedIndex++;
+                leftPartitionIndex++;
+            } else {
+                array[mergedIndex] = array[rightPartitionIndex];
+                mergedIndex++;
+                rightPartitionIndex++;
+            }
+        }
+
+        while(leftPartitionIndex < leftPartition.length) {
+            array[mergedIndex] = leftPartition[leftPartitionIndex];
+            mergedIndex++;
+            leftPartitionIndex++;
+        }
+
+        while(rightPartitionIndex <= right) {
+            array[mergedIndex] = array[rightPartitionIndex];
+            mergedIndex++;
+            rightPartitionIndex++;
+        }
+    }
+}
---
+++ src/test/java/sort/MergeSortTest.java
@@ -0,0 +1,8 @@
+package sort;
+
+public class MergeSortTest extends SortAlgorithmTest {
+    @Override
+    protected SortAlgorithm createInstance() {
+        return new MergeSort();
+    }
+}
